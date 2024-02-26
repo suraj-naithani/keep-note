c4f393c566c2d34e5ae6550ac0bccfa162971ac5
Suraj Nathani <66134800+Surajnathani@users.noreply.github.com>
2024-02-26 23:44:57 +0530

    Update Register.jsx

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/auth'
 import { toast } from 'react-toastify';
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const Register = () => {
     const [user, setUser] = useState({
